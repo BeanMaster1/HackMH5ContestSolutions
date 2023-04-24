19e6c135f3fee923c529950a02e63ff0338220dd
BeanMaster1 <105557386+BeanMaster1@users.noreply.github.com>
2023-04-24 13:31:17 -0500

    Update TowersOfAnya.java

--- TowersOfAnya.java
+++ TowersOfAnya.java
@@ -8,6 +8,7 @@ public class TowersOfAnya
     {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        //Move all rings from A to C using B as a pivot
         move(n, 'A', 'C', 'B');
     }
     public static void move(int n, char start, char end, char pivot)
@@ -16,8 +17,11 @@ public class TowersOfAnya
         {
             return;
         }
+        //Move everything but the bottom ring to the pivot
         move(n-1, start, pivot, end);
+        //Move the bottom ring to the end
         System.out.println("Move " + start + " to " + end);
+        //Move all the rings back onto the bottom ring, which is now at the end
         move(n-1, pivot, end, start);
     }
     
